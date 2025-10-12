574b0cfd6cbd6b01911e5c8cb5959076de373e47
joeribrinks <joeri1314@gmail.com>
2025-10-12 14:59:17 +0200

    added roles

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/nav";
+import Test from "./components/test";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Test />
     </div>
   );
 }
